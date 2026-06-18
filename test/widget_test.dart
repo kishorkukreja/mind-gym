b9f6c8233790635f1650f67ee3d6894910d3bdb6
Your Name <kishorkukreja76@gmail.com>
2026-06-18 23:17:43 +0100

    test: wrap smoke test with app provider

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,9 +1,19 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:mind_gym/main.dart';
+import 'package:mind_gym/services/app_provider.dart';
 
 void main() {
   testWidgets('Mind Gym smoke test', (WidgetTester tester) async {
-    await tester.pumpWidget(const MindGymApp());
+    SharedPreferences.setMockInitialValues({});
+
+    await tester.pumpWidget(
+      ChangeNotifierProvider(
+        create: (_) => AppProvider(),
+        child: const MindGymApp(),
+      ),
+    );
     await tester.pump();
     expect(find.byType(MindGymApp), findsOneWidget);
   });
